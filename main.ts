77365a6e530a4d3054f410bdc84d51d3fa24df44
cowdinosaur <akmal@tinkertanker.com>
2020-02-15 11:31:57 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -1,26 +1,19 @@
 /**
- * Types of tropical fruit
+ * Work with the GY-906 component.
  */
-enum TropicalFruit {
-    Banana = 0,
-    Pinapple = 1,
-    Coconut = 2
-}
+//% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
+namespace gy {
 
-/**
- * Pick some fruit and peel it.
- */
-namespace tropic {
     /**
-     * Pick a fruit
-     */
-    export function pick(fruit: TropicalFruit): boolean {
-        return true;
+    * TODO: initialization ring:bit car
+    * @param left describe parameter here, eg: AnalogPin.P1
+    * @param right describe parameter here, eg: AnalogPin.P2
+    */
+    //% weight=10
+    //% blockId=ringbitcar_init block="set left wheel at pin %left|right wheel at pin %right"
+    export function init_wheel(left: AnalogPin, right: AnalogPin): void {
+        // Add code here
+
     }
-    /**
-     * Peel the fruit if possible
-     */
-    export function peel(fruit: TropicalFruit): boolean {
-        return (fruit == TropicalFruit.Banana);
-    }
+
 }
